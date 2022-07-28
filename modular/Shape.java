b714dee487401e3c9d1830a7cc0db94e3409a0f9
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2022-07-28 14:50:23 -0500

    quick update

--- modular/Shape.java
+++ modular/Shape.java
@@ -37,44 +37,100 @@ class Ellipse extends Shape {
     }
 }
 
-class Body {
+class Body extends Ellipse{
     Shoulder leftShoulder;
     Shoulder rightShoulder;
-    Ellipse primary;
     public Body(Family family) {
         //default 20 weeks
-        primary = new Ellipse(new Dimension(new Point(0, 0, 0), family.size, family.size/2, family.size/2, 0, 0));
-        
+        super(new Dimension(new Point(0, 0, 0), family.size, family.size/2, family.size/2, 0, 0));
+        Dimension leftShoulderDim = new Dimension(new Point(0, family.size/2, 0), family.size/10, family.size/10, family.size/10, 0, 0);
+        Dimension rightShoulderDim = new Dimension(new Point(0, -family.size/2, 0), family.size/10, family.size/10, family.size/10, 0, 0);
+        leftShoulder = new Shoulder(family, leftShoulderDim, this);//TODO give dimensions!
+        rightShoulder = new Shoulder(family, rightShoulderDim, this);//TODO give dimensions!
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input) || leftShoulder.isIn(input) || leftShoulder.isIn(input));
     }
 }
 
-class Shoulder extends Ellipse {
-    Body body;
-    Bicep forearm;
+class Shoulder extends Ellipse{
+    Shape parent;
+    Bicep bicep;
     
-    public Shoulder() {
-        super();
+    public Shoulder(Family family, Dimension dim, Shape inParent) {
+        super(dim);
+        parent = inParent;
+        Dimension childDim = new Dimension(dim.point, family.size/10, family.size/2, family.size/10, ang1, ang2);
+        bicep = new Bicep(family, childDim, this);//TODO give dimensions!
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input) || bicep.isIn(input));
     }
 }
 
 class Bicep extends Ellipse{
-    Shoulder shoulder;
+    Shape parent;
     Elbow elbow;
+    
+    public Bicep(Family family, Dimension dim, Shape inParent) {
+        super(dim);
+        parent = inParent;
+        elbow = new Elbow(family, null, this);//TODO give dimensions!
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input) || elbow.isIn(input));
+    }
     
 }
 
 class Elbow extends Ellipse{
-    Bicep bicep;
+    Shape parent;
     Forearm forearm;
     
+    public Elbow(Family family, Dimension dim, Shape inParent) {
+        super(dim);
+        parent = inParent;
+        forearm = new Forearm(family, null, this);//TODO give dimensions!
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input) || forearm.isIn(input));
+    }
 }
 
 class Forearm extends Ellipse{
-    Hand shoulder;
-    Elbow elbow;
+    Shape parent;
+    Hand hand;
     
+    public Forearm(Family family, Dimension dim, Shape inParent) {
+        super(dim);
+        parent = inParent;
+        hand = new Hand(family, null, this);//TODO give dimensions!
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input) || hand.isIn(input));
+    }
 }
 
 class Hand extends Ellipse{
-    Forearm forearm;
+    Shape parent;
+    
+    public Hand(Family family, Dimension dim, Shape inParent) {
+        super(dim);
+        parent = inParent;
+    }
+    
+    @Override
+    public boolean isIn(Point input){
+        return (super.isIn(input));
+    }
 }
